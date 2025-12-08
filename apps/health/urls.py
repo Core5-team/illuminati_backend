0a4654a4cfaebaa705dc2affb52e8e86fecb0c3e
olehbulka <oleg.bulka@gmail.com>
2025-12-08 20:04:15 +0200

    CORE5-32: fix incorrect health path

--- apps/health/urls.py
+++ apps/health/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HealthView
 
 urlpatterns = [
-    path("api/health/", HealthView.as_view(), name="health"),
+    path("health/", HealthView.as_view(), name="health"),
 ]
