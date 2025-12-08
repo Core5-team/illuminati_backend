290eb7c8b0fa934b717e8b05673f6282acd45ead
olehbulka <oleg.bulka@gmail.com>
2025-12-08 17:11:26 +0200

    CORE5-32: update url

--- apps/health/urls.py
+++ apps/health/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HealthView
 
 urlpatterns = [
-    path("", HealthView.as_view(), name="health"),
+    path("api/health/", HealthView.as_view(), name="health"),
 ]
