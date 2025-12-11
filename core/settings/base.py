2f1d81e4ff6c0c785b3076a00c3d7617350882f1
olehbulka <oleg.bulka@gmail.com>
2025-12-11 14:42:57 +0200

    CORE5-32: update S3 variables to use os python module to get variables dynamically

--- core/settings/base.py
+++ core/settings/base.py
@@ -11,9 +11,9 @@ ALLOWED_HOSTS = ["*"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "shared", "images")
 
+AWS_S3_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME")
+AWS_S3_REGION = os.environ.get("AWS_S3_REGION")
 
-AWS_S3_BUCKET_NAME = "illuminati-backend-images"
-AWS_S3_REGION = "us-east-1"
 AWS_S3_URL = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_REGION}.amazonaws.com/"
 
 INSTALLED_APPS = [
